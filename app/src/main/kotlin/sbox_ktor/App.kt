bc9cc66a435684067983d92af371e547af08f0b8
Hikaru Inomoto <medalhkr@gmail.com>
2021-01-10 18:56:14 +0900

    format

--- app/src/main/kotlin/sbox_ktor/App.kt
+++ app/src/main/kotlin/sbox_ktor/App.kt
@@ -8,8 +8,9 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main(args: Array<String>) {
-    embeddedServer(Netty, 8080) {
+    val s =
+            embeddedServer(Netty, 8080) {
                 routing { get("/") { call.respondText("Hello, world!", ContentType.Text.Html) } }
             }
-            .start(wait = true)
+    s.start(wait = true)
 }
